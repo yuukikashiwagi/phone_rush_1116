0d7c57bf6462d675d601ed401260eb70451abc31
yuukikashiwagi <yuuki_kashiwagi@demia.co.jp>
2024-11-07 09:46:58 +0900

    ファイルの追加

---
+++ js/exercise.js
@@ -0,0 +1,15 @@
+function sumFilteredEvens(numbers) {
+    // 演習問題 1
+    // ここにコードを書いてください
+}
+
+const numbers = [2, 4, 5, 10, 12, 15, 20, 22];
+console.log(sumFilteredEvens(numbers)); // 関数の実行
+
+function isPrime(number) {
+    // 演習問題 2 
+    // ここにコードを書いてください
+}
+
+number = 5
+console.log(isPrime(number))
