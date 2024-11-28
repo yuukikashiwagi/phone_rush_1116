4d9914e8be052affbb3dcb3f2086a1013dd7ed70
yuukikashiwagi <yuuki_kashiwagi@demia.co.jp>
2024-11-28 22:28:37 +0900

    衝突判定なし

--- js/main.js
+++ js/main.js
@@ -396,7 +396,7 @@ function animate() {
     // ジャンプ関数の実行
     jump();
     // 衝突判定関数の実行
-    collision();
+    // collision();
     // カメラの移動
     camera.position.set(0, 8, player.position.z + 10);
     camera.lookAt(new Vector3(0, 5, player.position.z));
