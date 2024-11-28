4da082b470dc920c2a4dd2039ebf8c0aa7a452f0
yuukikashiwagi <yuuki_kashiwagi@demia.co.jp>
2024-11-29 00:06:15 +0900

    iosデバッグ終了

--- js/main.js
+++ js/main.js
@@ -241,9 +241,9 @@ document.addEventListener("DOMContentLoaded", function () {
   if (ios) {
     // iOS の時
     window.addEventListener("devicemotion", (dat) => {
-      aX = -1 * dat.accelerationIncludingGravity.x || 0;
-      aY = -1 * dat.accelerationIncludingGravity.y || 0;
-      aZ = -1 * dat.accelerationIncludingGravity.z || 0;
+      aX = dat.accelerationIncludingGravity.x || 0;
+      aY = dat.accelerationIncludingGravity.y || 0;
+      aZ = dat.accelerationIncludingGravity.z || 0;
     });
   } else {
     // android の時
