4c709c24b7cec4e7373e850c390be8a475babafb
yuukikashiwagi <yuuki_kashiwagi@demia.co.jp>
2024-11-28 23:42:36 +0900

    計算方法変更

--- js/main.js
+++ js/main.js
@@ -249,9 +249,9 @@ document.addEventListener("DOMContentLoaded", function () {
   } else {
     // android の時
     window.addEventListener("devicemotion", (dat) => {
-      aX = -dat.accelerationIncludingGravity.x || 0;
-      aY = -dat.accelerationIncludingGravity.y || 0;
-      aZ = -dat.accelerationIncludingGravity.z || 0;
+      aX = -1 * dat.accelerationIncludingGravity.x || 0;
+      aY = -1 * dat.accelerationIncludingGravity.y || 0;
+      aZ = -1 * dat.accelerationIncludingGravity.z || 0;
     });
   }
 
